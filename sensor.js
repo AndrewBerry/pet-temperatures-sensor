8da93b9ebe02284458b1953b3092a9baf8efe3df
Andrew Berry <andrew.berry@outlook.com>
2019-10-30 22:28:12 +1100

    init

---
+++ sensor.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const axios = require("axios");
+const sensor = require("node-dht-sensor").promises;
+
+const pets = require("./pets.json");
+
+async function pollSensors() {
+  const { temperature, humidity } = await sensor.read(22, 4);
+
+  await Promise.all(pets.map(async ({ petId, sensors }) => {
+    const readings = await Promise.all(sensors.map(async ({type, pin}) => {
+      return await sensor.read(type, pin);
+    }));
+
+    console.log(petId, readings);
+
+    function getMinMax(readings, isHigh) {
+      return readings.reduce((minMax, reading) => {
+        const { temperature } = reading;
+
+        if (minMax === null) {
+          return temperature;
+        }
+
+        return isHigh ?
+          Math.max(minMax, temperature) :
+          Math.min(minMax, temperature);
+      }, null);
+    }
+
+    const high = getMinMax(readings, true);
+    const low = getMinMax(readings, false);
+
+    const humiditySum = readings.reduce((humidity, reading) => {
+      return humidity + reading.humidity;
+    }, 0);
+    const humidity = humiditySum / readings.length;
+
+    axios.post(
+      `${process.env.FIREBASE_FUNCTIONS_BASE}/submitDataPoint`,
+      {
+        petId,
+        high,
+        low,
+        humidity
+      }
+    )
+  }));
+}
+
+setTimeout(pollSensors, 5000);
